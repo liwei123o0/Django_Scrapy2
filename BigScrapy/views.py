43b1d07e96d8dd2613b2ae674c5b919dd26910ba
XBZX <liwei429>
2018-02-12 16:35:30 +0800

    Start1.0

--- BigScrapy/views.py
+++ BigScrapy/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # ! /usr/bin/env python
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
@@ -10,3 +9,4 @@ def index(request):
 
 def index2(request):
     return render(request, 'index3.html')
+
